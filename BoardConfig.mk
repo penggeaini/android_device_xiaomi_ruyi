9cf2d6124250abff350431d037760f858fe13234
penggeaini <1807963290@qq.com>
2026-05-13 20:55:38 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # 内核（注意路径必须匹配）
 TARGET_PREBUILT_KERNEL := device/xiaomi/ruyi/kernel
