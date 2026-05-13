4929dc6927f1e87a58c5dba9131bfc7f5b5b0989
penggeaini <1807963290@qq.com>
2026-05-13 20:28:42 +0800

    Update twrp_ruyi.mk

--- twrp_ruyi.mk
+++ twrp_ruyi.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-PRODUCT_NAME := omni_ruyi
+PRODUCT_NAME := twrp_ruyi
 PRODUCT_DEVICE := ruyi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2405CPX3DC
